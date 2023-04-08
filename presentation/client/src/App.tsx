33c196d1c006b468b986c2c4670aaa5d9defc2c7
AjushRG <ayushregmi736@gmail.com>
2023-04-08 11:39:15 +0545

    organized our app in 3 layered architecture and typescript

---
+++ presentation/client/src/App.tsx
@@ -0,0 +1,19 @@
+// Importing modules
+import React from "react";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Importing local files
+import "./App.css";
+//import Home from "./pages/Home";
+//import Login from "./pages/Login";
+
+// App component
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
