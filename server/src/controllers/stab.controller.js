d9d0aeb3ca333e85c1a94b4d7e6cb5f3e23d2daa
AjushRG <ayushregmi736@gmail.com>
2023-04-25 12:16:10 +0545

    upload images into the cloud

--- server/src/controllers/stab.controller.js
+++ server/src/controllers/stab.controller.js
@@ -1,3 +1,4 @@
+// Import required modules
 import {
   getStabs,
   getStabById,
@@ -6,29 +7,41 @@ import {
   deleteStab,
 } from "../services/stab.service.js";
 import logger from "../utils/logger.js";
+import { uploadMedia } from "../middlewares/cloudinary.js";
 
+// Define a handler for creating a stab
 export const createStabHandler = async (req, res) => {
   const stab = req.body;
+
   try {
+    // Call the createStab function from the stab.service.js module
     const createdStab = await createStab(stab);
+    // Send a response with a 201 status code and the created stab data
     res.status(201).json({
       message: "Stab created successfully",
       data: createdStab,
     });
   } catch (error) {
+    // Log the error and send a response with a 409 status code and the error message
     logger.error(error);
     res.status(409).json({ message: error.message });
   }
 };
 
+// Define a handler for retrieving all stabs
 export const getStabsHandler = async (req, res) => {
   try {
+    // Call the getStabs function from the stab.service.js module
     const stabs = await getStabs();
+    // If no stabs are found, throw an error
+    if (!stabs) throw new Error("No stabs found");
+    // Send a response with a 200 status code and the retrieved stabs data
     res.status(200).json({
       message: "Stabs retrieved successfully",
       data: stabs,
     });
   } catch (error) {
+    // Log the error and send a response with a 404 status code and the error message
     logger.error(error);
     res.status(404).json({
       message: error.message,
@@ -36,22 +49,29 @@ export const getStabsHandler = async (req, res) => {
   }
 };
 
+// Define a handler for retrieving a single stab by ID
 export const getStabByIdHandler = async (req, res) => {
   const id = req.params.id;
   try {
+    // Call the getStabById function from the stab.service.js module with the provided ID
     const stab = await getStabById(id);
+    // If no stab is found with the provided ID, throw an error
     if (!stab) throw new Error(`Stab with id ${id} not found`);
+    // Send a response with a 200 status code and the retrieved stab data
     res.status(200).json({
       message: `Stab with id ${id} retrieved successfully`,
       data: stab,
     });
   } catch (error) {
+    // Log the error and send a response with a 404 status code and the error message
     logger.error(error);
     res.status(404).json({ message: error.message });
   }
 };
 
+// Define a handler for updating a stab by ID
 export const updateStabHandler = async (req, res) => {
+  // Get the ID from the request parameters
   const id = req.params.id;
   const stab = req.body;
   try {
--- server/src/routes/stab.routes.js
+++ server/src/routes/stab.routes.js
@@ -1,5 +1,7 @@
+// Import the express module
 import express from "express";
 
+// Import the controller functions for handling Stab routes
 import {
   createStabHandler,
   getStabsHandler,
@@ -8,12 +10,15 @@ import {
   deleteStabHandler,
 } from "../controllers/stab.controller.js";
 
+// Create an express router instance
 const router = express.Router();
 
+// Define the Stab routes
 router.post("/stabs", createStabHandler);
 router.get("/stabs", getStabsHandler);
 router.get("/stabs/:id", getStabByIdHandler);
 router.put("/stabs/:id", updateStabHandler);
 router.delete("/stabs/:id", deleteStabHandler);
 
+// Export the router as the default module export
 export default router;
